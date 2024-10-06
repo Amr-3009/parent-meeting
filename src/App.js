ea5445cd712e3fb1663258eb24528c5a9f37d830
Amr-3009 <amrosama1k@gmail.com>
2024-10-06 21:29:20 +0300

    Update file extensions for Navbar, Footer, and MainBody components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
-import Navbar from "./Components/Navbar/Navbar";
-import Footer from "./Components/Footer/Footer";
-import MainBody from "./Components/MainBody/MainBody";
+import Navbar from "./Components/Navbar/Navbar.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
+import MainBody from "./Components/MainBody/MainBody.jsx";
 
 function App() {
   return (
